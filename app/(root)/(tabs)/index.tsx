0eb7936b49410f4e458d093e14ba79254fd4408e
naman-rathore99 <namanrathore59@gmail.com>
2025-03-05 00:06:48 +0530

    project init

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Link href="/Sign-in">Sign In</Link>
+      <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore </Link>
       <Link href="/profile">Profile </Link>
       <Link href="/properties/1">Properties </Link>
